c1c1b2c04791ad50dbb3ef1ed9af90073231b32c
Fabrizio <fabri10dematias@hotmail.com>
2025-05-28 19:21:09 -0300

    upload proyect

--- src/main/java/org/example/presupuesto/HelloApplication.java
+++ src/main/java/org/example/presupuesto/HelloApplication.java
@@ -9,7 +9,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 900);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
 
         stage.setTitle("Remito - Dicor");
         stage.setScene(scene);
